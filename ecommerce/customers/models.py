0ed51daa5dba0ad06dd3ab9e1328b3a3d16fd590
mojtaba-sfrz <msafarzayee@gmail.com>
2023-07-26 01:03:43 +0330

    Authenticated user added

--- ecommerce/customers/models.py
+++ ecommerce/customers/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class AuthenticatedCustomer(models.User):
+    """
+    a user who already authenticated
+    """
+    updated_at = models.DateTimeField(null=True)
+    deleted_at = models.DateTimeField(null=True)
+    phone_number = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.get_full_name()
+
+    class Meta:
+        verbose_name = _("old customer")
+        ordering = ["first_name", "last_name"]
+
+
+
+
+
