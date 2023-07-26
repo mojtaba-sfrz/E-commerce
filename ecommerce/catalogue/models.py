fba6eff44bb35c617388f5ec455c242761d20a1b
mojtaba-sfrz <msafarzayee@gmail.com>
2023-07-26 13:22:03 +0330

    Product app added to catalogue app and tow fields of Authenticated customer in customer app has been modified

--- ecommerce/catalogue/models.py
+++ ecommerce/catalogue/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _, pgettext_lazy
 
-# Create your models here.
+
+class AbstractProduct():
+
+    title = models.CharField(pgettext_lazy('Product title', 'Title'),
+                             max_length=255, blank=True)
+    description = models.TextField(_('Description'), blank=True)
+    rating = models.FloatField(_('Rating'), null=True, editable=False)
+    date_created = models.DateTimeField(_("Date created"), auto_now_add=True)
+    date_updated = models.DateTimeField(_("Date Updated", auto_now=True))
+
+    class Meta:
+        ordering = ['-date_created']
+        verbose_name = _('Product')
+        verbose_name_plural = _('Products')
+
+    def __str__(self):
+
+        return self.title
+
--- ecommerce/customers/models.py
+++ ecommerce/customers/models.py
@@ -1,14 +1,15 @@
 from django.db import models
+from django.contrib.auth import models as user_model
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
 
-class AuthenticatedCustomer(models.User):
+class AuthenticatedCustomer(user_model.User):
     """
     a user who already authenticated
     """
-    updated_at = models.DateTimeField(null=True)
-    deleted_at = models.DateTimeField(null=True)
+    date_created = models.DateTimeField(_("Date created"), auto_now_add=True)
+    date_updated = models.DateTimeField(_("Date Updated", auto_now=True))
     phone_number = models.CharField(max_length=15)
 
     def __str__(self):
@@ -17,6 +18,8 @@ class AuthenticatedCustomer(models.User):
     class Meta:
         verbose_name = _("old customer")
         ordering = ["first_name", "last_name"]
+
+
 
 
 
