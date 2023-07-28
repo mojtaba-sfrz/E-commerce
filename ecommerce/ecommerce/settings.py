ff9826e625b4a7631f101fd8884384cf06a62e6c
mojtaba-sfrz <msafarzayee@gmail.com>
2023-07-28 03:40:27 +0330

    Add apps to installed apps list in settings.py file

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'catalogue',
+    'customers',
+    'dashboard',
+    'delivery',
+    'order',
+    'payment',
+    'suppliers',
 ]
 
 MIDDLEWARE = [
